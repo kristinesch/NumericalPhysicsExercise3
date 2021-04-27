fixed total mass calculation

functions.py
@@ -109,8 +109,8 @@ def minAndMaxConcentrations(C):#C is the array returned after simulation
     maxC=np.amax(C,axis=1) #max value of each column of C
     return minC,maxC
 
-def totalMass(C,L):
-    masses=np.sum(C,axis=1)*12*360e12
+def totalMass(C,dz): #not sure if this is right???
+    masses=np.sum(C,axis=1)*12*360e12*dz
     return masses
 
 """convergence test"""

problem3.py
@@ -124,5 +124,12 @@ deepSimC=np.load("fullDeepSim2.npy")
 
 timesteps=int(T/dt)
 t=np.linspace(0,T,timesteps)
-deepSimMasses=totalMass(deepSimC, L)
+deepSimMasses=totalMass(deepSimC, dz)
 plotTotalMass(deepSimMasses, t)
+
+def calculateMassPerYear(masses, years):
+    return (masses[-1]-masses[0])/years
+
+print(deepSimMasses[-1],deepSimMasses[0])
+
+print("Average total mass absorbed by the global oceans per year: ", calculateMassPerYear(deepSimMasses, 10))